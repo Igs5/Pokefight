c41ee72bea42f8c2770773e5badf3a935cd648f7
unknown <ruthcllp@gmail.com>
2024-06-04 21:01:44 +0200

    assigned colours and gradients conditionally

--- client/src/components/PokeList.jsx
+++ client/src/components/PokeList.jsx
@@ -5,6 +5,36 @@ import axios from "axios"
 const PokeList=()=>{
     const [pokemons, setPokemons] = useState([]);
 
+    const typeColors = {
+        grass: 'bg-green-500',
+        water: 'bg-blue-500',
+        fire: 'bg-red-500',
+        electric: 'bg-yellow-500',
+        bug: 'bg-green-300',
+        normal: 'bg-gray-300',
+        poison: 'bg-purple-500',
+        ground: 'bg-yellow-700',
+        'grass-poison': 'bg-gradient-to-r from-green-500 to-purple-500',
+        'flying-fire': 'bg-gradient-to-r from-red-500 to-yellow-500',
+        'normal-flying': 'bg-gradient-to-r from-gray-300 to-pink-300',
+        'bug-poison': 'bg-gradient-to-r from-green-300 to-purple-700',
+        // Colours to be changed
+      };
+
+      const getBackgroundClass = (types) => {
+        // Handle single type
+        if (types.length === 1) {
+          return typeColors[types[0]] || 'bg-white';
+        }
+        // Handle two types
+        if (types.length === 2) {
+          const combinedType = `${types[0]}-${types[1]}`;
+          return typeColors[combinedType] || `bg-gradient-to-r from-${typeColors[types[0]]} to-${typeColors[types[1]]}`;
+        }
+        // Default background
+        return 'bg-white';
+      };
+      
   useEffect(() => {
     async function fetchData() {
       try {
@@ -39,11 +69,15 @@ const PokeList=()=>{
  
 
     return(
-        <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
            <h1>Pokémon List</h1>
            <ul>
              {pokemons.map(pokemon => (
-                <div key={pokemon.id} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div key={pokemon.id} 
+                className={`max-w-sm border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-4 ${
+                    getBackgroundClass(pokemon.types)
+                  }`}
+                >
                <li key={pokemon.name }>
                 <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{pokemon.name.charAt(0).toUpperCase()+pokemon.name.slice(1)}</h3>
                 <img src={pokemon.image} alt={pokemon.name} className="rounded-t-lg" />
